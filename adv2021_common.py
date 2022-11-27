21d2643bc55233f2dd166d86c1b1033da4ec48c3
Hector Bravo <hbravo@cuic.net>
2022-11-27 18:27:24 +0100

    Fixed and refactored Day 5_1

--- adv2021_common.py
+++ adv2021_common.py
@@ -20,3 +20,31 @@ def print_result(result, expected_result = None):
 
 def str_list_to_int_lst(str_list):
     return list(map(int, str_list))
+
+def gen_point_list(vector):
+    # print('vector:', vector)
+    x0 = vector[0]
+    y0 = vector[1]
+    x1 = vector[2]
+    y1 = vector[3]
+
+    direction_x = 0
+    direction_y = 0
+
+    if x0 > x1:
+        direction_x = -1
+    elif x0 < x1:
+        direction_x = 1
+    if y0 > y1:
+        direction_y = -1
+    elif y0 < y1:
+        direction_y = 1
+
+    max_steps_x = abs(x0 - x1) + 1
+    max_steps_y = abs(y0 - y1) + 1
+    num_steps = max(max_steps_x, max_steps_y)
+
+    point_list = []
+    for step in range(num_steps):
+        point_list.append((x0 + step * direction_x, y0 + step *direction_y))
+    return point_list
