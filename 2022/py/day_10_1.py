daa3e188b9d4863890d44594c085039a808bfb74
Hector Bravo <hbravo@cuic.net>
2022-12-11 04:35:06 +0100

    Day 10 2022

---
+++ 2022/py/day_10_1.py
@@ -0,0 +1,34 @@
+# Copyright Hector Bravo <hbravo@cuic.net>
+# Code for Day 10 excercise 1 https://adventofcode.com/2022/day/10
+
+import sys
+sys.path.append('../../common/py')
+import adv_common as common
+
+@common.elapsed_time_factory()
+def process_solution(contents):
+    # print('Contents:', contents)
+    signal_strenght = 0
+    cycles = 0
+    register_x = 1
+    for instruction in contents:
+        cycles += 1
+        if cycles == 20 or ((cycles - 20) % 40 == 0):
+            signal_strenght += (cycles*register_x)
+            print(instruction)
+            print('cycles, register, signal_strenght', cycles, register_x, signal_strenght)
+        if instruction != 'noop':
+            instruction, value = [int(x) if x.lstrip("-").isnumeric() else x for x in instruction.split()]
+            print(instruction, value)
+            cycles += 1
+            if cycles == 20 or ((cycles - 20) % 40 == 0):
+                signal_strenght += (cycles*register_x)
+                print('cycles, register, signal_strenght', cycles, register_x, signal_strenght)
+            register_x += value
+        # print('cycles, register', cycles, register_x)
+    return signal_strenght
+
+if __name__ == "__main__":
+    contents = common.read_input()
+    result = process_solution(contents)
+    common.print_result(result, 12560)
---
+++ 2022/py/day_10_2.py
@@ -0,0 +1,42 @@
+# Copyright Hector Bravo <hbravo@cuic.net>
+# Code for Day 10 excercise 2 https://adventofcode.com/2022/day/10
+
+import sys
+sys.path.append('../../common/py')
+import adv_common as common
+import numpy as np
+
+CRT_COLS = 40
+CRT_ROWS = 6
+
+@common.elapsed_time_factory()
+def process_solution(contents):
+    # print('Contents:', contents)
+    cycles = 0
+    register_x = 1
+    crt_string = ''
+    for instruction in contents:
+        if (cycles % CRT_COLS) in range(register_x - 1, register_x + 2):
+            crt_string += '#'
+        else:
+            crt_string += ' '
+        # print(crt_string)
+        cycles += 1
+        if instruction != 'noop':
+            if (cycles % CRT_COLS) in range(register_x - 1, register_x + 2):
+                crt_string += '#'
+            else:
+                crt_string += ' '
+            instruction, value = [int(x) if x.lstrip("-").isnumeric() else x for x in instruction.split()]
+            # print(instruction, value)
+            cycles += 1
+            register_x += value
+        # print('cycles, register', cycles, register_x)
+    message_array = np.array([crt_string[CRT_COLS*i:CRT_COLS*i+CRT_COLS] for i in range(CRT_ROWS)])
+    print(message_array)
+    return 0
+
+if __name__ == "__main__":
+    contents = common.read_input()
+    result = process_solution(contents)
+    # common.print_result(result, 12560)
