05025c47723410aba4af0a7570e1172ea4293be8
Hector Bravo <hbravo@cuic.net>
2023-01-04 18:21:52 +0100

    Improved version and fix

--- 2021/py/day_6_1.py
+++ 2021/py/day_6_1.py
@@ -2,6 +2,7 @@
 # Code for Day 6 excercise 1 https://adventofcode.com/2021/day/6
 
 import adv_common as common
+import collections as cl
 
 DAYS_TO_SIMULATE = 80
 
@@ -18,14 +19,35 @@ def calc_len_after_days(fish_list, days):
         # print('day:', day, 'fish_list:', fish_list)
     return len(fish_list)
 
+def calc_len_after_days_optimal(fish_list, days):
+    collection = cl.Counter(
+        fish for fish in fish_list
+    )
+    for day in range(days):
+        _new_collection = cl.Counter()
+        for age, fishes in collection.items():
+            if age == 0:
+                _new_collection[6] += fishes
+                _new_collection[8] += fishes
+            else:
+                _new_collection[age - 1] += fishes
+        collection = _new_collection
+        # print('day:', day, 'fish_list:', collection)
+    return collection.total()
+
 @common.elapsed_time_factory()
 def process_solution(contents):
     # print('Contents:', contents)
-    fish_list = common.str_list_to_int_lst(contents[0].split(','))
-    # print(fish_list)
-    return calc_len_after_days(fish_list, DAYS_TO_SIMULATE)
+    return calc_len_after_days(list(contents), DAYS_TO_SIMULATE)
+
+@common.elapsed_time_factory()
+def process_solution2(contents):
+    # print('Contents:', contents)
+    return calc_len_after_days_optimal(contents, DAYS_TO_SIMULATE)
 
 if __name__ == "__main__":
-    contents = common.read_input()
+    contents = common.read_input(data_type = 'int_list', separator = ',')
     result = process_solution(contents)
+    common.print_result(result, 362639)
+    result = process_solution2(contents)
     common.print_result(result, 362639)
--- 2021/py/day_6_2.py
+++ 2021/py/day_6_2.py
@@ -2,25 +2,14 @@
 # Code for Day 6 excercise 1 https://adventofcode.com/2021/day/6
 
 import adv_common as common
+import day_6_1
 
 DAYS_TO_SIMULATE = 256
-
-def calc_len_after_days(number, days):
-    # print(number, days)
-    count = 1
-    if days > 0:
-        for days_remaining in range(days, 0, -1):
-            if (number == 0):
-                count += calc_len_after_days(8, days_remaining - 1)
-                number = 6
-            else:
-                number -= 1
-    return count
 
 @common.elapsed_time_factory()
 def process_solution(contents):
     # print('Contents:', contents)
-    return sum([calc_len_after_days(number, DAYS_TO_SIMULATE) for number in contents])
+    return day_6_1.calc_len_after_days_optimal(contents, DAYS_TO_SIMULATE)
 
 if __name__ == "__main__":
     contents = common.read_input(data_type = 'int_list', separator = ',')
