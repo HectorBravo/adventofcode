50131937c98bf907cf9e5b114c44a6d64d55a3a9
Hector Bravo <hbravo@cuic.net>
2023-03-15 02:35:43 +0100

    Day 2 2020

---
+++ 2020/day_2_1.py
@@ -0,0 +1,24 @@
+# Copyright Hector Bravo <hbravo@cuic.net>
+# Code for Day 2 excercise 1 https://adventofcode.com/2020/day/2
+
+import adv_common as common
+import re
+
+@common.elapsed_time_factory()
+def process_contents(contents):
+    # print(contents)
+
+    number = 0
+    for content in contents:
+        minimum, maximum, letter, password = re.fullmatch(r'(\d+)-(\d+) (.): (\w+)', content).groups()
+        minimum, maximum = int(minimum), int(maximum)
+        # print(minimum, maximum, letter, password, password.count(letter))
+
+        if password.count(letter) in list(range(minimum, maximum + 1)):
+            number += 1
+    return number
+
+if __name__ == "__main__":
+    contents = common.read_input('str')
+    result = process_contents(contents)
+    common.print_result(result, 454)
---
+++ 2020/day_2_2.py
@@ -0,0 +1,24 @@
+# Copyright Hector Bravo <hbravo@cuic.net>
+# Code for Day 2 excercise 2 https://adventofcode.com/2020/day/2
+
+import adv_common as common
+import re
+
+@common.elapsed_time_factory()
+def process_contents(contents):
+    # print(contents)
+
+    number = 0
+    for content in contents:
+        lower, higher, letter, password = re.fullmatch(r'(\d+)-(\d+) (.): (\w+)', content).groups()
+        lower, higher = int(lower), int(higher)
+        # print(lower, higher, letter, password, password[lower-1] == letter, password[higher-1] == letter)
+
+        if (password[lower-1] == letter) ^ (password[higher-1] == letter):
+            number += 1
+    return number
+
+if __name__ == "__main__":
+    contents = common.read_input('str')
+    result = process_contents(contents)
+    common.print_result(result, 649)
